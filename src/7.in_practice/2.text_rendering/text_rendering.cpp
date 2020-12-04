7a183305a4ffd81c380cf86a710fdb793b9e67b3
Aleks Kamko <aykamko@gmail.com>
2020-12-04 14:01:39 -0500

    increase GPU buffer size

--- src/7.in_practice/2.text_rendering/text_rendering.cpp
+++ src/7.in_practice/2.text_rendering/text_rendering.cpp
@@ -162,7 +162,7 @@ int main()
     glGenBuffers(1, &VBO);
     glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 6 * 4, NULL, GL_DYNAMIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 6 * 4 * 8000000, NULL, GL_DYNAMIC_DRAW);
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(float), 0);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
